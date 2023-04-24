85f74e8c31cf26cf8e953af5991a541ce87afe14
Kasper Skytte Andersen <github@kasper.slmail.me>
2023-04-24 10:30:23 +0200

    auto unlock op vault before chezmoi apply

--- home/.chezmoiscripts/run_before_loginsync_op.sh
+++ home/.chezmoiscripts/run_before_loginsync_op.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-#when running chezmoi commands, especially apply, unlocking the Bitwarden vault
-#doesn't pass on the BW_SESSION var, so it's best done beforehand to avoid
-#unlocking it every single time bw is used
-# if command -v op >/dev/null 2>&1
-# then
-#   if [ "$(op account list)" == "" ]
-#   then
-#     echo "No 1password accounts, please add one (use address: my.1password.com)..."
-#     eval $(op account add --signin)
-#   elif ! op whoami 2> /dev/null
-#   then
-#     echo "1password is not signed in, please sign in..."
-#     eval $(op signin)
-#   fi
-# elif ! command -v op >/dev/null 2>&1
-# then
-#   echo "op (1password CLI) is not installed or available in \$PATH. Please install!"
-# fi
+#unlocks op before chezmoi apply
+#may need to check for this: https://developer.1password.com/docs/cli/app-integration#turn-on-the-app-integration-and-sign-in-to-your-account
+if command -v op >/dev/null 2>&1
+then
+  # if getent group onepassword-cli
+  # then
+  #   sudo chgrp onepassword-cli "${BINDIR}/op"
+  #   chmod g+s "${BINDIR}/op"
+  # fi
+  op update
+  if [ "$(op account list)" == "" ]
+  then
+    echo "No 1password accounts, please add one (using address: my.1password.com)..."
+    eval $(op account add --address my.1password.com --signin)
+  elif ! op whoami >/dev/null 2>&1
+  then
+    echo "1password is not signed in, please sign in..."
+    eval $(op signin)
+  fi
+elif ! command -v op >/dev/null 2>&1
+then
+  echo "op (1password CLI) is not installed or available in \$PATH"
+fi
