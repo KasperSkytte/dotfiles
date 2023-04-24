36082e70525773e75dab29310fc9b0635bf9eb3f
Kasper Skytte Andersen <github@kasper.slmail.me>
2023-04-24 10:44:06 +0200

    op signin is idempotent

--- home/.chezmoiscripts/run_before_loginsync_op.sh
+++ home/.chezmoiscripts/run_before_loginsync_op.sh
@@ -13,9 +13,7 @@ then
   then
     echo "No 1password accounts, please add one (using address: my.1password.com)..."
     eval $(op account add --address my.1password.com --signin)
-  elif ! op whoami >/dev/null 2>&1
-  then
-    echo "1password is not signed in, please sign in..."
+  else
     eval $(op signin)
   fi
 elif ! command -v op >/dev/null 2>&1
